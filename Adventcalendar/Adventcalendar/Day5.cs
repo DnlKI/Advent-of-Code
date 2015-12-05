d65b9db17b6d70c9ca9f5e2a4575a118afcb14f2
Daniel Schwietert <danielschwietert@web.de>
2015-12-05 14:10:54 +0100

    Added day 5

---
+++ Adventcalendar/Adventcalendar/Day5.cs
@@ -0,0 +1,101 @@
+﻿using System.IO;
+
+namespace Adventcalendar
+{
+    class Day5
+    {
+        public static string Part1()
+        {
+            string input = File.ReadAllText(@"..\..\Day5.txt").Replace("\r", "");
+            string[] strings = input.Split('\n');
+            int niceStrings = 0;
+
+            foreach (string s in strings)
+            {
+                int vovels = 0;
+                char lastChar = ' ';
+                int doubleLetters = 0;
+
+                foreach (char c in s)
+                {
+                    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    {
+                        vovels++;
+                    }
+
+                    if (c == lastChar)
+                    {
+                        doubleLetters++;
+                    }
+
+                    lastChar = c;
+                }
+
+                if (vovels < 3 || doubleLetters < 1)
+                {
+                    continue;
+                }
+
+                if (s.Contains("ab") || s.Contains("cd") || s.Contains("pq") || s.Contains("xy"))
+                {
+                    continue;
+                }
+
+                niceStrings++;
+            }
+
+            return niceStrings.ToString();
+        }
+
+        public static string Part2()
+        {
+            string input = File.ReadAllText(@"..\..\Day5.txt").Replace("\r", "");
+            string[] strings = input.Split('\n');
+            int niceStrings = 0;
+
+            foreach (string s in strings)
+            {
+                bool hasTwoPairs = false;
+                bool hasRepeatedLetter = false;
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (i > 0)
+                    {
+                        string pair = s[i - 1].ToString() + s[i].ToString();
+                        string before = "";
+                        string after = "";
+
+                        for (int j = 0; j < s.Length; j++)
+                        {
+                            if (j < i - 1)
+                            {
+                                before += s[j];
+                            } else if (j > i)
+                            {
+                                after += s[j];
+                            }
+                        }
+
+                        if (before.Contains(pair) || after.Contains(pair))
+                        {
+                            hasTwoPairs = true;
+                        }
+                    }
+                    
+                    if (i < s.Length - 2 && s[i] == s[i + 2])
+                    {
+                        hasRepeatedLetter = true;
+                    }
+                }
+
+                if (hasTwoPairs && hasRepeatedLetter)
+                {
+                    niceStrings++;
+                }
+            }
+
+            return niceStrings.ToString();
+        }
+    }
+}
--- Adventcalendar/Adventcalendar/Program.cs
+++ Adventcalendar/Adventcalendar/Program.cs
@@ -6,10 +6,11 @@ namespace Adventcalendar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 1: Part 1: " + Day1.Part1() + ". Floor; Part 2: " + Day1.Part2() + " Position");
+            Console.WriteLine("Day 1: Part 1: " + Day1.Part1() + ". Floor; Part 2: " + Day1.Part2() + ". Position");
             Console.WriteLine("Day 2: Part 1: " + Day2.Part1() + " Square feet of paper; Part 2: " + Day2.Part2() + " Feet of ribbon");
             Console.WriteLine("Day 3: Part 1: " + Day3.Part1() + " Houses; Part 2: " + Day3.Part2() + " Houses");
-            Console.WriteLine("Day 4: Part 1: " + Day4.Part1() + " Number; Part 2: " + Day4.Part2() + " Number");
+            //Console.WriteLine("Day 4: Part 1: " + Day4.Part1() + " Number; Part 2: " + Day4.Part2() + " Number");                         // takes too long
+            Console.WriteLine("Day 5: Part 1: " + Day5.Part1() + " Nice strings; Part 2: " + Day5.Part2() + " Nice strings");
             Console.Read();
         }     
     }
